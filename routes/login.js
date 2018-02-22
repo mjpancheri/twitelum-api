d31c15dedf59c11ac9e3b9b1d84ec3edee80f2b7
Mario Souto <soutomarios@gmail.com>
2018-02-22 19:32:52 -0300

    update login

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,40 @@
-// const authLogin = require('../middlewares/authLogin')
+const errors = require('restify-errors');
 
 module.exports = (app) => {
     const authLogin = app.middlewares.authLogin
-    app.post('/login', (req, res) => {
+    app.post('/login', (req, res, next) => {
+            const usuarioDAO = app.infra.dao.UsuariosDAO
             const jsonBody = typeof req.body === 'object' ? req.body : JSON.parse(req.body)
             const login = jsonBody.login
             const senha = jsonBody.senha
-            
-            const token = authLogin.geraToken(login, senha)
 
-            res.json({ token })
+
+            if(jsonBody.login && jsonBody.senha) {
+                usuarioDAO
+                    .buscarPorLoginESenha({
+                        login,
+                        senha
+                    })
+                    .then((usuario) => {
+                        if(!usuario) {
+                            return next( new errors.NotFoundError('Usuario não encontrado') )
+                        }
+                        const token = authLogin.geraToken(login, senha)
+                        res.json({ token })    
+                    })
+            } 
+
+            if(!jsonBody.login && !jsonBody.senha) {
+                return next( new errors.InvalidContentError('É necessário informar um login e uma senha para efetuar login no sistema') )    
+            } else {
+                if(!jsonBody.login) {
+                    return next( new errors.InvalidContentError('É necessário informar um login para efetuar login no sistema') )
+                }
+                if(!jsonBody.senha) {
+                    return next( new errors.InvalidContentError('É necessário informar uma senha para efetuar login no sistema') )
+                }
+            }
+            
         }
     )
 
