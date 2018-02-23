2d89b10c0de81e6a1408b4de459926c9b3dc4206
Mario Souto <soutomarios@gmail.com>
2018-02-23 15:20:41 -0300

    update cors

--- config/app.js
+++ config/app.js
@@ -1,21 +1,24 @@
 const restify = require('restify')
 const consign = require('consign')
 const errors = require('restify-errors')
-const corsMiddleware = require('restify-cors-middleware')
+// const corsMiddleware = require('restify-cors-middleware')
  
-const cors = corsMiddleware({
-  preflightMaxAge: 5, //Optional
-  origins: ['*'],
-  allowHeaders: ['X-AUTH-TOKEN'],
-  exposeHeaders: ['API-Token-Expiry']
-})
+// const cors = corsMiddleware({
+//   preflightMaxAge: 5, //Optional
+//   origins: ['*'],
+//   allowHeaders: ['X-AUTH-TOKEN', 'Access-Control-Allow-Origin'],
+//   exposeHeaders: ['X-AUTH-TOKEN', 'Access-Control-Allow-Origin']
+// })
  
 const app = restify.createServer()
 
 require('dotenv').config()
 
-app.pre(cors.preflight)
-app.use(cors.actual)
+var cors = require('cors');
+
+app.use(cors());
+// app.pre(cors.preflight)
+// app.use(cors.actual)
 
 app.use(restify.plugins.bodyParser({ mapParams: false }));
 app.use(restify.plugins.queryParser());
